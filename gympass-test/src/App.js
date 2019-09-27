8edf7d2d418add154ba9a08ee40ce40182e2b278
Diogo da Silva Santana <diogo.silva@proartproducoes.com.br>
2019-09-26 23:31:51 -0300

    Novo

--- gympass-test/src/App.js
+++ gympass-test/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
-import Container from '@material-ui/core/Container';
-
-import GridRepos from './components/gridRepos/gridRepos';
-import HeaderComponent from './components/header/header';
+import HeaderComponent from './components/Header/header';
 
 function App() {
-  return (
-    <div className="App">
-      <Container>
-        <HeaderComponent></HeaderComponent>
-        <GridRepos></GridRepos>
-      </Container>
-    </div>
-  );
+
+	return (
+		<div className="App">
+			<HeaderComponent></HeaderComponent>
+		</div>
+	);
 }
 
 export default App;
--- gympass-test/src/components/header/header.js
+++ gympass-test/src/components/header/header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './Header.css'
+import logo from './logo.svg';
 
 export default class HeaderComponent extends Component {
-  render() {
+
+	title = "Teste de Front End"
+
+	render() {
     return (
-      <header>
-          Header
+			<header>
+				<img src={logo} className="Header-logo" alt="logo" />
+				<h1>{this.title}</h1>
       </header>
     );
   }
